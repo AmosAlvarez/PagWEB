660a98856d52f82016d68ac29b7bb0968350d33e
AmosAlvarez <amos.alvarez@hotmail.com>
2019-05-28 23:10:50 +0200

    Add files via upload

---
+++ Página WEB/main.js
@@ -0,0 +1,70 @@
+// Variables
+
+var nav = document.getElementById('nav');
+var menu = document.getElementById('enlaces');
+var abrir = document.getElementById('open');
+var botones = document.getElementsByClassName('btn-header');
+var cerrado = true;
+
+
+function menus(){
+    var Desplazamiento_Actual = window.pageYOffset;
+
+    if(Desplazamiento_Actual <= 300){
+        nav.classList.remove('nav2');
+        nav.className = ('nav1');
+        nav.style.transition = '1s';
+        abrir.style.color = '#fff';
+    }else{
+        nav.classList.remove('nav1');
+        nav.className = ('nav2');
+        nav.style.transition = '1s';
+        abrir.style.color = '#000';
+    }
+}
+
+function apertura(){
+    if(cerrado){
+        menu.style.width = '70vw';
+        cerrado = false;
+    }else{
+        menu.style.width = '0%';
+        menu.style.overflow = 'hidden';
+        cerrado = true;
+    }
+}
+
+window.addEventListener('load', function(){
+    $('#onload').fadeOut();
+    $('body').removeClass('hidden');
+    menus();
+});
+
+window.addEventListener('click',function(e){
+    console.log(e.target);
+    if(cerrado==false){
+        let span = document.querySelector('span');
+        if(e.target !== span && e.target !== abrir){
+            menu.style.width = '0%';
+            menu.style.overflow = 'hidden';
+            cerrado = true;
+        }
+    }
+});
+
+window.addEventListener('scroll', function(){
+    console.log(window.pageYOffset);
+    menus();
+});
+
+window.addEventListener('resize', function(){
+    if(screen.width>= 700){
+        cerrado = true;
+        menu.style.removeProperty('overflow');
+        menu.style.removeProperty('width');
+    }
+});
+
+abrir.addEventListener('click', function(){
+    apertura();
+});
